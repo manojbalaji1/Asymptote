a14902b3aaeae3ca1aafd8577e467b52263f7414
Manoj Balaji J <manojbalaji1@gmail.com>
2016-07-17 11:21:30 +0530

    virtualw

---
+++ virtualawyer/index.php
@@ -0,0 +1,366 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="A chat-bot based legal assistant, which provides legal solutions to all the common queries which a common man faces">
+    <meta name="keywords" content="Virtual legal advisor, law, virtual laywer, legal, legal advisor, advisor, chat-bot, chat query, law query, query, legal query, law porn">
+    <meta name="author" content="Saurabh Chalke, manoj balaji, anvesh garg">
+
+    <title>Your personal virtual lawyer</title>
+    <link rel="icon" href="img/lawWeight.png" type="image/png">
+    <!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/freelancer.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+   
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"> </script>
+
+    <style type="text/css">
+
+	p {
+		font-family: helvetica;
+		text-align: justify;
+	}
+    li {
+        font-family: helvetica;
+        font-size: 150%;
+    }
+    a:hover {
+        color:#708090;
+    }
+    </style>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body id="page-top" class="index">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#page-top">Virtual lawyer</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right" style="font-size:70%">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#page-top" style="hover: #708090">About</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#portfolio" style="hover: #708090">Demo</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#contact">Contact us</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <!-- Header -->
+
+    <header style="background-color:#fff">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-4 col-xs-10 col-xs-offset-1">
+                    <img class="img-responsive img-circle" src="img/vectorJudge.jpg" alt="">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1 col-xs-10 col-xs-offset-1">
+                    <div class="intro-text">
+                        <span class="name" style="color: #2C3E50; size:50%; text-transform: none;">Virtual Legal Advisor</span>
+                    </div>
+                        <hr class="star-primary">
+                    <div class="intro-text">
+                        <p style="color:#506070; text-align: center"><br>
+                        In a world where law and order means everything, this tool is just the thing!<br>virtual legal assistance now at your fingertips!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!--
+    <header style="background-image: url('img/1103.jpg')">
+        <div class="container" >
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1 text-center">
+                   <p class="intro-text"><q><i>Operating systems are like underwear — nobody really wants to look at them.</i></q><br><span style="position:absolute;right: 0px;text-align:right;"> - Bill Joy (co-founder of Sun Microsystems)</span></p>
+                </div>
+            </div>
+
+        </div>
+        </div>
+    </header>
+    -->
+    <!-- Portfolio Grid Section -->
+    <hr style="height: 10px;
+    border: 0;
+    box-shadow: 0 10px 10px -10px #708090 inset;">
+    <section id="portfolio">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Alpha Demo</h2>
+                    <hr class="star-primary">
+                </div>                
+            </div>
+            <br />
+            <div class="row">
+            	<p class="col-lg-offset-1 col-lg-10" style="text-align:center; font-family: helvetica">
+            	The product being in it's alpha stage right now solves some of the basic offenses of the human body enlisted under the Indian Penal Code. many new patch are on the way. Give yourself a demo of the budding chat-bot based legal advisor!
+           <br /><br/>
+            	</p>
+            	
+            	<p class="col-lg-offset-1 col-lg-10" style="text-align:center; font-family: helvetica">
+            		 <br /><strong>Click on the button given below to </strong>
+            	</p>
+            </div>
+            <br />
+            <br />
+            <div class="row">
+                
+                <div class="col-lg-12 portfolio-item">
+                    <a href="#portfolioModal1" class="portfolio-link" data-toggle="modal"> 
+                        <div class="caption">
+                            <div class="caption-content">
+                                <i class="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                       <img src="img/buttonLink.jpg" class="img-responsive img-rounded text-center" alt="">
+                    </a>
+                    
+                </div>
+                
+            </div>
+        </div>
+    </section>
+
+   
+    
+    <!-- Contact Section -->
+    <hr style="height: 10px;
+    border: 0;
+    box-shadow: 0 10px 10px -10px #708090 inset;">
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Contact Me</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+            <div class="row">
+                                <br><br>
+                           
+                        
+                <div class="col-lg-12 col-xs-12">
+                    <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
+                    <!-- The form should work on most web servers, but if the form is not working you may need to configure your web server differently. -->
+                    <form name="sentMessage" id="contactForm" action="" method="post">
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Name</label>
+                                <input type="text" class="form-control" placeholder="Name" name="name" id="name" required data-validation-required-message="Please enter your name.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Email Address</label>
+                                <input type="email" class="form-control" placeholder="Email Address" name="email" id="email" required data-validation-required-message="Please enter your email address.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Message</label>
+                                <textarea rows="5" class="form-control" placeholder="Message" name="message" id="message" required data-validation-required-message="Please enter a message."></textarea>
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <br>
+                        <div id="success"></div>
+                        <div class="row">
+                            <div class="form-group col-xs-12">
+                                <button type="submit" class="btn btn-primary btn-lg">Send</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="text-center">
+        <div class="footer-below">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        Copyright &copy; 2016 | Saurabh Chalke, Anvesh Garg, Manoj Balaji
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-top page-scroll visible-xs visible-sm">
+        <a class="btn btn-primary" href="#page-top">
+            <i class="fa fa-chevron-up"></i>
+        </a>
+    </div>
+
+    <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <div class="modal-body">
+                            <h2>Lawyer me up!</h2>
+                            <hr class="star-primary">
+                            <br />
+                            
+                             <br /><br />
+                            <form name="sentMessage" id="contactForm" action="" method="post">
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Name</label>
+                                <input type="text" class="form-control" placeholder="Query string" name="queryString" id="queryString" required data-validation-required-message="Please enter your name.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        	<br /><br />
+                            	<div id="success"></div>
+                        <div class="row">
+                            <div class="form-group col-xs-12">
+                                <button  id="queryButton" type="submit" class="btn btn-primary btn-lg">Send</button>
+                            </div>
+                        </div>
+                    </form>
+                    <p id="output">
+                    </p>
+                    <br />
+                    <br />
+                    <br />
+                    <ul style="font-size: 60%; text-align: left">
+                                <li>
+                                   *Still in the alpha version
+                                </li>
+                                <li>
+                                   **Query should involve laws affecting the human body
+                                </li>
+                       </ul>   
+                            <br />
+
+                            
+                            <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+ 
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/cbpAnimatedHeader.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <!--<script src="js/contact_me.js"></script>-->
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/freelancer.js"></script>
+     <script type="text/javascript">
+    $(document).ready(function() {
+    $('#queryButton').click(function(e) {
+    	e.preventDefault();
+      postString();
+    });
+    function postString()
+    {
+    	var string=$("queryString").val();
+    	//check if its empty
+    	
+    	
+    		$.ajax
+    		({
+    				type: "POST",
+                    //SEND TO MY SERVER URL
+                    url: "/cgi-gin/getChat.py",
+                    data: {'query': string},
+                    //console.log(data);
+                    success: function (response) {
+                   	 console.log("the string passed is: " + response);
+                   	 
+                   	 	$('#output').append(response.string);
+                   	 
+                    }
+    		});
+    		
+    	}
+    
+});
+</script>
+<!--    <script type = "text/javascript">
+         $(document).ready(function() {
+			
+            $("#queryButton").click(function(event){
+               $.getJSON('sample.json', function(jd) {
+                  $('#output').html('<p> Name: ' + jd.name + '</p>');
+                  $('#output').append('<p>Age : ' + jd.age+ '</p>');
+                  $('#output').append('<p> Sex: ' + jd.sex+ '</p>');
+               });
+            });
+				
+         });
+      </script>
+      -->
+    
+</body>
+
+</html>
